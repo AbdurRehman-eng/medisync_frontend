2ce8b7c49d657703d6c8d0fdfb2180eaf73bc272
Abd Ur Rehman Shafique <shafiqueabdurrehman1@gmail.com>
2024-11-16 00:03:32 +0500

    Abd Update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import HomeScreen from "./pages/home/page";
-import Login from "./pages/login/page";
-import Signup from "./pages/signup/page";
 
 export default function Home() {
   return (
